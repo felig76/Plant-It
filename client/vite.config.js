a566b8d5429841381c80cffa06c17ee8bcd09111
Matiasjs1 <162461241+Matiasjs1@users.noreply.github.com>
2025-10-08 13:00:17 -0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 // Configuración Vite con proxy hacia el backend
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 👈 AÑADE ESTA LÍNEA
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_TARGET, // Puerto arbitrario para el backend
+        target: process.env.VITE_API_TARGET,
         changeOrigin: true,
         secure: false,
       }
     }
-  } 
+  }
 })
