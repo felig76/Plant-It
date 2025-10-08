65e32b871bf9c24ac629861112027cabb133e663
Matiasjs1 <162461241+Matiasjs1@users.noreply.github.com>
2025-10-08 14:38:43 -0300

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,42 +1,40 @@
 // Importar dependencias
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const helmet = require('helmet')
-const cookieParser = require('cookie-parser')
-const errorHandler = require('./middlewares/errorHandler')
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const errorHandler = require('./middlewares/errorHandler');
+require('dotenv').config();
+
+const userRoutes = require('./routes/user.routes');
+const plantRoutes = require('./routes/plant.routes');
+const connectDB = require('./config/db');
+connectDB();
+
+const app = express();
+
 const allowedOrigins = [
-  'https://plant-it-l-kb81.onrender.com', // tu frontend en Render
+  'https://plant-it-l-kb81.onrender.com',
+  'http://localhost:5173' // opcional, para pruebas locales
 ];
-require('dotenv').config()
 
-// Import rutas
-const userRoutes = require('./routes/user.routes')
-const plantRoutes = require('./routes/plant.routes')
-
-// Crear la aplicación
-const app = express()
-
-// Conectar a la base de datos
-const connectDB = require('./config/db')
-connectDB()
-
-// Middleware
 app.use(cors({
   origin: allowedOrigins,
-  credentials: true, // permite enviar cookies o encabezados de autorización
+  credentials: true,
 }));
-app.use(express.json())  // Para analizar el cuerpo de las solicitudes JSON
-app.use(helmet())  // Protege la aplicación de ataques comunes
-app.use(morgan('dev'))  // Registra las solicitudes HTTP en la consola
-app.use(cookieParser())  // Analiza las cookies de las solicitudes
 
-// Api
-app.use('/api/users', userRoutes)
-app.use('/api/plants', plantRoutes)
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(cookieParser());
 
-// Middleware de manejo de errores
-app.use(errorHandler)  // Maneja los errores de la aplicación
+// Rutas
+app.use('/api/users', userRoutes);
+app.use('/api/plants', plantRoutes);
 
+// Middleware de errores
+app.use(errorHandler);
 
-module.exports = app
+module.exports = app;
